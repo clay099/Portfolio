16a7017963cad8e95e36def37b222827ea92116c
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-10-30 13:18:05 -0400

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Navbar, Nav, Image } from "react-bootstrap";
+
+const NavBarComp = () => {
+	return (
+		<Navbar bg="light" expand="md">
+			<Navbar.Brand href="#home">
+				<Image
+					alt=""
+					src={process.env.PUBLIC_URL + "profileImg.jfif"}
+					width="30"
+					height="30"
+					className="d-inline-block align-top"
+					roundedCircle
+				/>{" "}
+				Clayton Whittaker
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#about">About</Nav.Link>
+					<Nav.Link href="#projects">Projects</Nav.Link>
+					<Nav.Link href="#skills">Skills</Nav.Link>
+					<Nav.Link href="#experience">Experience</Nav.Link>
+					<Nav.Link href="#contact">Contact</Nav.Link>
+					<Nav.Link href="#Resume">Resume</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+};
+
+export default NavBarComp;
