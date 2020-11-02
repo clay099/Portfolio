da3f6e3454fba031c733c3d9cb419234733e62a7
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-11-02 11:59:26 -0500

    updated styling for specific p

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,22 +5,22 @@ import "./About.css";
 
 const About = () => {
 	return (
-		<section id="about">
+		<section id="About">
 			<Card className="shadow p-3 mb-5 bg-white rounded">
 				<SectionHead title="About Me" />
-				<p>
+				<p className="About-body">
 					I am a Software Engineer who believes that technology can
 					improve the user experience to ensure client satisfaction
 					and deliver sustained business growth.
 				</p>
-				<p>
+				<p className="About-body">
 					I started my professional career working in the financial
 					services industry in Australia. During this time i focused
 					on enhancing existing technology efficiencies. This
 					experience led to a real passion for technology and
 					automation.
 				</p>
-				<p>
+				<p className="About-body">
 					I always seek opportunities and challenges that can provide
 					a meaningful improvement to our ever day lives.
 				</p>
