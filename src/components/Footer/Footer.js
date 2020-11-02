78d6a42eb50b7ec58e8c24a488e97ed240f6e748
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-11-02 15:16:47 -0500

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,45 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
+	let year = new Date().getFullYear();
 	return (
-		<footer class="footer">
-			<div class="container">
-				<span class="text-muted">Clayton Whittaker</span>
+		<footer className="footer">
+			<div className="container footer-row">
+				<span className="text-muted">Clayton Whittaker ©{year}</span>
+				<a
+					href="https://www.linkedin.com/in/clayton-whittaker/"
+					target="_blank"
+				>
+					<div class="flex icon" id="icon-2">
+						<i class="fab fa-linkedin"></i>
+					</div>
+				</a>
+				<a href="https://github.com/clay099" target="_blank">
+					<div class="flex icon" id="icon-3">
+						<i class="fab fa-github"></i>
+					</div>
+				</a>
 			</div>
 		</footer>
 	);
 };
 
 export default Footer;
+
+<div class="icon-wrap flex row">
+	<a href="https://www.linkedin.com/in/mattwilliams85">
+		<div class="flex icon" id="icon-2">
+			<i class="mdi mdi-linkedin"></i>
+		</div>
+	</a>
+	<a href="https://www.facebook.com/matthew.williams.351">
+		<div class="flex icon" id="icon-3">
+			<i class="mdi mdi-facebook"></i>
+		</div>
+	</a>
+	<a href="https://instagram.com/infinite_edge/">
+		<div class="flex icon" id="icon-4">
+			<i class="mdi mdi-instagram"></i>
+		</div>
+	</a>
+</div>;
