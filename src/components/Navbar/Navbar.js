8b737857ce25aeadab0c95ade90392b970e8425c
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-11-04 09:53:13 -0500

    added resume link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,9 +21,12 @@ const NavBarComp = () => {
 					<Nav.Link href="#about">About</Nav.Link>
 					<Nav.Link href="#projects">Projects</Nav.Link>
 					<Nav.Link href="#skills">Skills</Nav.Link>
-					<Nav.Link href="#experience">Experience</Nav.Link>
-					<Nav.Link href="#contact">Contact</Nav.Link>
-					<Nav.Link href="#resume">Resume</Nav.Link>
+					<Nav.Link
+						href={process.env.PUBLIC_URL + "resume.pdf"}
+						target="_blank"
+					>
+						Resume
+					</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
